9a59e2e1baf5bdd5abc6a907b3212f567bfd508d
Mateus Rodolfo <mateus.rodolfo.1998@gmail.com>
2023-05-25 10:49:29 +0200

    converted code to use const and SMatrix

--- BoseHubbard/rust/src/main.rs
+++ BoseHubbard/rust/src/main.rs
@@ -1,28 +1,28 @@
 // usage: `bose_hubbard < system.yaml`
+const N_MAX: usize = 50;
+const RESOLUTION: usize = 500;
+const INITIAL_GUESS: f64 = 1.0;
+const TOL: f64 = 1E-4;
+const ITER: usize = 500;
 
-use std::{
-    fs::File,
-    io::{read_to_string, stdin, Write},
-    time::SystemTime,
-};
+use std::{fs::File, io::Write, time::SystemTime};
 
 use itertools_num::linspace;
-use nalgebra::{DMatrix, DVector, SymmetricEigen};
+use nalgebra::{SMatrix, SVector, SymmetricEigen};
 use rayon::prelude::*;
-use serde::{Deserialize, Serialize};
 
 fn main() {
     // Reading system
-    let input = read_to_string(stdin().lock()).expect("could not read file");
-    let system = System::from_str(input);
-    println!("\nParsed system:\n");
-    system.show();
+    // let input = read_to_string(stdin().lock()).expect("could not read file");
+    // let system = System::from_str(input);
+    // println!("\nParsed system:\n");
+    // SYSTEM.show();
     println!();
 
     // Solving system
     println!("Solving...\n");
     let time = SystemTime::now();
-    let answer = system.solve();
+    let answer = solve();
     let elapsed = time.elapsed().unwrap().as_secs();
     println!("Solved in {elapsed} seconds\n");
 
@@ -43,83 +43,60 @@ fn main() {
     println!("Done");
 }
 
-/// Holds the parameters for solving the system
-#[derive(Serialize, Deserialize, Debug)]
-struct System {
-    n_max: usize,
-    resolution: usize,
-    initial_guess: f64,
-    tol: f64,
-    iter: usize,
-}
+/// Solves the system
+fn solve() -> Vec<Vec<f64>> {
+    // Create local operators
 
-impl System {
-    /// Instantiates a `System` from a yaml string in stdin
-    fn from_str(s: String) -> Self {
-        serde_yaml::from_str(&s).expect("could not parse json file")
-    }
+    // a
+    let a: SMatrix<f64, N_MAX, N_MAX> = SMatrix::from_fn(|i, j| {
+        if i + 1 == j {
+            ((i + 1) as f64).sqrt()
+        } else {
+            0.0
+        }
+    });
 
-    /// Prints `System` to stout
-    fn show(&self) {
-        println!("{self:#?}");
-    }
+    // a_dag
+    let a_dag = a.transpose();
 
-    /// Solves the system
-    fn solve(&self) -> Vec<Vec<f64>> {
-        // Create local operators
+    // n
+    let n = &a_dag * &a;
 
-        // a
-        let a = DMatrix::from_fn(self.n_max, self.n_max, |i, j| {
-            if i + 1 == j {
-                ((i + 1) as f64).sqrt()
-            } else {
-                0.0
-            }
+    // identity
+    let identity: SMatrix<f64, N_MAX, N_MAX> = SMatrix::identity();
+
+    // compute psi
+    let mut psi_mat: Vec<Vec<f64>> = vec![vec![0.0; RESOLUTION]; RESOLUTION];
+    // let mut psi_mat = DMatrix::zeros(self.resolution, self.resolution);
+
+    // Generate the range of values
+    let ti: Vec<f64> = linspace(0.0, 0.05, RESOLUTION).collect();
+    let mui: Vec<f64> = linspace(0.0, 3.0, RESOLUTION).collect();
+
+    // fill psi_mat matrix using rayon / parallel loop
+    psi_mat.par_iter_mut().enumerate().for_each(|(k2, row)| {
+        let mu = mui[k2];
+        row.iter_mut().enumerate().for_each(|(k1, psi)| {
+            let t = ti[k1];
+            *psi = find_psi(t, mu, &a, &a_dag, &n, &identity).abs();
         });
+    });
 
-        // a_dag
-        let a_dag = a.transpose();
-
-        // n
-        let n = &a_dag * &a;
-
-        // identity
-        let identity = DMatrix::<f64>::identity(self.n_max, self.n_max);
-
-        // compute psi
-        let mut psi_mat: Vec<Vec<f64>> = vec![vec![0.0; self.resolution]; self.resolution];
-        // let mut psi_mat = DMatrix::zeros(self.resolution, self.resolution);
-
-        // Generate the range of values
-        let ti: Vec<f64> = linspace(0.0, 0.05, self.resolution).collect();
-        let mui: Vec<f64> = linspace(0.0, 3.0, self.resolution).collect();
-
-        // fill psi_mat matrix using rayon / parallel loop
-        psi_mat.par_iter_mut().enumerate().for_each(|(k2, row)| {
-            let mu = mui[k2];
-            row.iter_mut().enumerate().for_each(|(k1, psi)| {
-                let t = ti[k1];
-                *psi = find_psi(t, mu, &a, &a_dag, &n, &identity, self).abs();
-            });
-        });
-
-        // return
-        psi_mat
-    }
+    // return
+    psi_mat
 }
 
 /// Find psi
 fn find_psi(
     t: f64,
     mu: f64,
-    a: &DMatrix<f64>,
-    a_dag: &DMatrix<f64>,
-    n: &DMatrix<f64>,
-    identity: &DMatrix<f64>,
-    system: &System,
+    a: &SMatrix<f64, N_MAX, N_MAX>,
+    a_dag: &SMatrix<f64, N_MAX, N_MAX>,
+    n: &SMatrix<f64, N_MAX, N_MAX>,
+    identity: &SMatrix<f64, N_MAX, N_MAX>,
 ) -> f64 {
-    let mut iter = system.iter;
-    let mut guess = system.initial_guess;
+    let mut iter = ITER;
+    let mut guess = INITIAL_GUESS;
 
     // Get BHMF Hamiltonian
     let bhmf_ham = get_bhmf_ham(t, mu, guess, a, a_dag, n, identity);
@@ -129,7 +106,7 @@ fn find_psi(
     let mut psi = eigenvector.transpose() * a * eigenvector;
 
     // converge
-    while (psi[0] - guess).abs() > system.tol && iter != 0 {
+    while (psi[0] - guess).abs() > TOL && iter != 0 {
         guess = psi[0];
         let bhmf_ham = get_bhmf_ham(t, mu, guess, a, a_dag, n, identity);
         let eigenvector = get_eigen(bhmf_ham);
@@ -146,11 +123,11 @@ fn get_bhmf_ham(
     t: f64,
     mu: f64,
     psi: f64,
-    a: &DMatrix<f64>,
-    a_dag: &DMatrix<f64>,
-    n: &DMatrix<f64>,
-    identity: &DMatrix<f64>,
-) -> DMatrix<f64> {
+    a: &SMatrix<f64, N_MAX, N_MAX>,
+    a_dag: &SMatrix<f64, N_MAX, N_MAX>,
+    n: &SMatrix<f64, N_MAX, N_MAX>,
+    identity: &SMatrix<f64, N_MAX, N_MAX>,
+) -> SMatrix<f64, N_MAX, N_MAX> {
     let term1 = -6.0 * t * (psi * (a + a_dag) - psi.powf(2.0) * identity);
     let term2 = 0.5 * n * (n - identity);
     let term3 = -mu * n;
@@ -159,7 +136,7 @@ fn get_bhmf_ham(
 }
 
 /// Calculates eigenvector
-fn get_eigen(ham: DMatrix<f64>) -> DVector<f64> {
+fn get_eigen(ham: SMatrix<f64, N_MAX, N_MAX>) -> SVector<f64, N_MAX> {
     // Calculate eigenvalues
     let eigen = SymmetricEigen::new(ham);
 
